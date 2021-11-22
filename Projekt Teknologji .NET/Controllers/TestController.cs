17ca6b6a58865a671bd5bb006f3423ccef94d964
Admirim Korici <82025628+admirimkorici@users.noreply.github.com>
2021-11-22 15:09:47 +0100

    Shtimi i Controllerit "Test"

---
+++ Projekt Teknologji .NET/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projekt_Teknologji.NET.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AfishoEmrin(string emri, string mbiemri)
+        {
+            string msg;
+            msg = String.Format("Emri eshte " + emri + " Mbiemri eshte " + mbiemri);
+            return Content(msg);
+        }
+    }
+}
