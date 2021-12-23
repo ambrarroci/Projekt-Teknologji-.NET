baa9d55d012fb2ef7a04d60efad247d1ad76d80c
Admirim Korici <82025628+admirimkorici@users.noreply.github.com>
2021-12-23 15:28:37 +0100

    Ndryshime-23/12/2021

--- Projekt Teknologji .NET/App_Start/BundleConfig.cs
+++ Projekt Teknologji .NET/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace Projekt_Teknologji.NET
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/Style.css"));
         }
     }
 }
---
+++ Projekt Teknologji .NET/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projekt_Teknologji.NET.Models
+{
+    [Table("Contact")]
+    public class Contact
+    {
+        [Required]
+        public string Emri { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Subjekti { get; set; }
+        [Required]
+        public string Mesazhi { get; set; }
+    }
+}
