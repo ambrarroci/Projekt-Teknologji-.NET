d9218c6fdb45e622b96864ff5b1800a6c86d71b6
iamGeri <gprifti93@gmail.com>
2022-01-18 15:32:10 +0100

    Modelet

---
+++ Projekt Teknologji .NET/Models/Rezervimet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace Projekt_Teknologji.NET.Models
+{
+    public class Rezervimet
+    {
+        [Required]
+        public int IdRezervimi { get; set; }
+
+        [Required]
+        [Display(Name = "Date Rezervimi")]
+        public DateTime Date_Rezervimi { get; set; }
+
+        [Required]
+        [Display(Name = "Date Kthimi")]
+        public DateTime Date_kthimi { get; set; }
+
+        [Required]
+        public bool ERezervuar { get; set; }
+
+        [Required]
+        public decimal Pagesa_totale { get; set; }
+
+        [ForeignKey("UserId")]
+        public ApplicationUser User {get; set; }
+    }
+}
---
+++ Projekt Teknologji .NET/Models/Tipi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Projekt_Teknologji.NET.Models
+{
+    public class Tipi
+    {
+        [Required]
+        public int ID{ get; set; }
+
+        [Required]
+        [Display(Name = "Emri")]
+        public string Emri { get; set; }
+        [Required]
+        [Display(Name = "Ikona")]
+        public string Ikona { get; set; }
+    }
+}
